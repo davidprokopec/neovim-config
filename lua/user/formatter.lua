0254f67429198fd9e2705178dc7fb1eb24690e86
David Prokopec <prokopec.david@seznam.cz>
2023-11-24 18:25:00 +0100

    fixeq

--- lua/user/formatter.lua
+++ lua/user/formatter.lua
@@ -1,28 +1,30 @@
-local conform = require('conform')
+local conform = require("conform")
 
 local opts = {
+  formatters = {
+  },
+  -- The default formatter can be specified here
   formatters_by_ft = {
-    lua = { 'stylua' },
+    lua = { "stylua" },
     -- Conform will run multiple formatters sequentially
-    python = { 'isort', 'black' },
+    python = { "isort", "black" },
     -- Use a sub-list to run only the first available formatter
-    javascript = { { 'biome', 'prettierd' } },
-    typescript = { { 'biome', 'prettierd' } },
-    javascriptreact = { { 'biome', 'prettierd' } },
-    typescriptreact = { { 'biome', 'prettierd' } },
-
+    javascript = { { "biome", "prettierd" } },
+    typescript = { { "biome", "prettierd" } },
+    javascriptreact = { { "biome", "prettierd" } },
+    typescriptreact = { { "biome", "prettierd" } },
   },
 }
 
 conform.setup(opts)
 
-vim.api.nvim_create_user_command('Format', function(args)
+vim.api.nvim_create_user_command("Format", function(args)
   local range = nil
   if args.count ~= -1 then
     local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
     range = {
       start = { args.line1, 0 },
-      ['end'] = { args.line2, end_line:len() },
+      ["end"] = { args.line2, end_line:len() },
     }
   end
   conform.format({ async = true, lsp_fallback = true, range = range })
