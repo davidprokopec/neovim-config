3fe2b5a26295c3a807001c649ec561e921a86e13
David Prokopec <prokopec.david@seznam.cz>
2023-11-24 18:26:58 +0100

    removed lazygit, but added it back i dont know dude

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -103,7 +103,7 @@ return packer.startup(function(use)
 
   -- Git
   use({ "lewis6991/gitsigns.nvim" })
-  -- use({ "kdheepak/lazygit.nvim" })
+  use({ "kdheepak/lazygit.nvim" })
 
   -- Github copilot
   use("github/copilot.vim")
